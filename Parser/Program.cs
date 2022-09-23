c28b8bc3d2af3e542165f5492bcf23b662f2bc16
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-09-23 11:10:06 -0500

    Add project files.

---
+++ Parser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+string path = @"C:\Users\hello\OneDrive\Documents\IO-Lab.txt";
+
+FileInfo newFile = new FileInfo(path);
+
+string line = "initial";
+
+using (StreamReader reader = newFile.OpenText())
+{
+    line = reader.ReadLine();
+    while (String.IsNullOrWhiteSpace(line))
+    {
+
+        string[] words = line.Split('.');
+        foreach (string word in words)
+        {
+            using (StreamWriter writer = newFile.CreateText())
+            {
+                writer.WriteLine("STOP");
+            }
+
+        }
+        line = reader.ReadLine();
+    }
+    newFile.CreateAsSymbolicLink(@"C:\Users\hello\OneDrive\Documents\IO-Lab.txt");
+}
